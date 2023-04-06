01d44960b591cb97c98dbcf72f2dfe95fefeb84a
Abbas <tundeeey@gmail.com>
2023-04-06 05:50:52 +0100

    adding and styling with js function of phone(), dialNumber() and gotoHome() function

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ let hour = document.getElementById("hour");
 let minutes = document.getElementById("minutes");
 let display = document.getElementById("show-key");
 let dele = document.getElementById("delete");
+let showAddNumber = document.getElementById("add-number-text");
+let callNumber = document.getElementById("green-call-btn-div");
+let callSection = document.getElementById("call-section-div");
+let phoneCall = document.getElementById("call");
 
 
 dele.style.display = "none";
+showAddNumber.style.display = "none";
+callSection.style.display = "none";
+
 
 
 
@@ -28,14 +35,27 @@ function showKey(key) {
     setInterval(() => {
         if (display.value === "") {
             dele.style.display = "none";
-    
+            showAddNumber.style.display = "none";
+
         }
     }, 1000);
     display.value += key;
     dele.style.display = "block";
-
+    showAddNumber.style.display = "block";
 }
 
 function del() {
     display.value = display.value.slice(0, -1);
+}
+
+function phone() {
+    callSection.style.display = "block";
+}
+
+function dialNumber() {
+
+}
+
+function gotoHome() {
+    callSection.style.display = "none";
 }
