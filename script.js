73f1148d5599e9e11c91dc24ee9e1e8eb0808cf4
Abbas <tundeeey@gmail.com>
2023-04-13 16:41:56 +0100

    was able to fix my phone camera to be working, but it does take picture yet, i'm working on the takepix button for it to be able to take a picture and then will also work on how to save the snapped picture in a storage

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let hour = document.getElementById("hour");
+let hourCL = document.getElementById("hourCL");
+let hourCS = document.getElementById("hourCS");
+let minutesCL = document.getElementById("minutesCL");
+let minutesCS = document.getElementById("minutesCS");
 let minutes = document.getElementById("minutes");
 let display = document.getElementById("show-key");
 let dele = document.getElementById("delete");
@@ -10,11 +14,15 @@ let callingNumber = document.getElementById("calling-number");
 let callStatus = document.getElementById("call-status");
 let callingSection = document.getElementById("calling-section");
 
+let cameraSection = document.getElementById('camera-section');
+
+
 
 dele.style.display = "none";
 showAddNumber.style.display = "none";
 callSection.style.display = "none";
 callingSection.style.display = "none";
+cameraSection.style.display = 'none';
 
 
 
@@ -23,10 +31,16 @@ callingSection.style.display = "none";
 function time() {
     let date = new Date();
     hour.innerHTML = date.getHours();
+    hourCL.innerHTML = date.getHours();
+    hourCS.innerHTML = date.getHours();
     minutes.innerHTML = date.getMinutes();
+    minutesCL.innerHTML = date.getMinutes();
+    minutesCS.innerHTML = date.getMinutes();
 
     if (minutes.innerHTML < 10) {
         minutes.innerHTML = "0" + minutes.innerHTML;
+        minutesCL.innerHTML = "0" + minutesCL.innerHTML;
+        minutesCS.innerHTML = "0" + minutesCS.innerHTML;
     }
 
     if (date.getHours() == 0) {
@@ -60,6 +74,8 @@ function phone() {
 function dialNumber() {
     callingSection.style.display = "block";
     callSection.style.display = "none";
+    callStatus.innerHTML = "calling..."
+    callingNumber.innerHTML = display.value;
     display.value = "";
 
 }
@@ -71,12 +87,26 @@ function gotoHome() {
 callingNumber.innerHTML = "";
 callingNumber.innerHTML = display.value;
 
-callStatus.innerHTML = "calling..."
-function abortCall() {    
-    // callStatus.innerHTML = "call ended";
+
+function abortCall() {
+    callStatus.innerHTML = "call ended...";
     setTimeout(() => {
         callingSection.style.display = "none";
     }, 2000);
     callSection.style.display = "block";
-    
+
 }
+
+let video = document.getElementById('camera-screen');
+let canvas = document.getElementById('canvas');
+
+async function startCamera() {
+    cameraSection.style.display = 'block';
+    let thePix = await navigator.mediaDevices.getUserMedia({audio: false, video:true})
+    video.srcObject = thePix;
+}
+
+// function takePix() {
+//     canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+
+// }
